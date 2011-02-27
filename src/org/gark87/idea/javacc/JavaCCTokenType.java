8e058741bc0bd643b3dd49a3b3347dc38564cee3
gark87 <my_another@mail.ru>
2011-02-28 02:40:48 +0300

    delete old files. now all they are generated by jj2IntelliJ

--- src/org/gark87/idea/javacc/JavaCCTokenType.java
+++
@@ -1,9 +0,0 @@
-package org.gark87.idea.javacc;
-
-import com.intellij.psi.JavaTokenType;
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.tree.java.IJavaElementType;
-
-public interface JavaCCTokenType extends JavaTokenType {
-    IElementType SHARP = new IJavaElementType("NUMBER_SIGN");
-}
